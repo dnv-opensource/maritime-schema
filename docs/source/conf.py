25c64e1c214305a821459adc194374a852e8a188
Claas <claas.rostock@dnv.com>
2024-02-28 17:07:21 +0100

    bumped version number to 0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, DNV AS. All rights reserved."
 author = "Minos Hemrich, Tom Arne Pedersen, Claas Rostock"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.3"
+release = "0.0.4"
 
 # -- General configuration ---------------------------------------------------
 
