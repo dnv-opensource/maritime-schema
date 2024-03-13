d7704fa082cd04fa74cdee90d573944b8609cd6b
Minos Hemrich <minos.hemrich@dnv.com>
2024-03-13 14:34:27 +0100

    update to ruff formatting

--- src/maritime_schema/types/caga.py
+++ src/maritime_schema/types/caga.py
@@ -11,6 +11,7 @@ from uuid import UUID, uuid4
 from pydantic import BaseModel, ConfigDict
 from pydantic.fields import Field  # , FieldInfo
 
+# change
 from maritime_schema.types.caga_examples import (
     create_caga_config_example,
     create_caga_data_example,
@@ -202,9 +203,11 @@ class Initial(BaseModelConfig):
 
 class DataPoint(BaseModelConfig):
     value: Optional[float] = Field(None, description="the value of the data at the current timestep", examples=[12.3])
-    m_before_leg_change: float = Field(None, description="meters before the waypoint to start interpolating to the new value", examples=[10]
+    m_before_leg_change: float = Field(
+        None, description="meters before the waypoint to start interpolating to the new value", examples=[10]
     )
-    m_after_leg_change: Optional[float] = Field(None, description="meters after the waypoint to finish interpolating to the new value", examples=[10]
+    m_after_leg_change: Optional[float] = Field(
+        None, description="meters after the waypoint to finish interpolating to the new value", examples=[10]
     )
     interp_method: Optional[Union[InterpolationMethod, str]] = Field(None, description="Method used for interpolation")
 
@@ -242,7 +245,9 @@ class Waypoint(BaseModelConfig):
     position: Position = Field(
         description="A geographical coordinate", examples=[Position(latitude=51.2123, longitude=11.2313)]
     )
-    turn_radius: Optional[float] = Field(None, description="Orthodrome turn radius as defined in RTZ format", examples=[200])
+    turn_radius: Optional[float] = Field(
+        None, description="Orthodrome turn radius as defined in RTZ format", examples=[200]
+    )
     data: Data = Field(None, description="A `Data` object that includes `speed`, `course`, and `heading` data points")
 
 
